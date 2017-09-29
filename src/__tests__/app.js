12a9dcba84ebfe097caa526e04ab276af787642d
Adam Woodbury <adamwoodbury@gmail.comm>
2017-09-28 18:29:31 -0700

    add more tests

--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -5,23 +5,34 @@ import App from '../App'
 let wrapper
 
 beforeEach(() => {
-    wrapper = mount(App)
-})
-
-// without nexttick (all snapshots are identical)
-
-test('!a && !b', () => {
-    wrapper.vm.a = false
-    wrapper.vm.b = false
-
-    expect(wrapper.vm.a).toBe(false)
-    expect(wrapper.vm.b).toBe(false)
-    expect(wrapper.html()).toMatchSnapshot()
+  wrapper = mount(App)
 })
 
 test('a && !b', () => {
     wrapper.vm.a = true
     wrapper.vm.b = false
+
+    expect(wrapper.vm.a).toBe(true)
+    expect(wrapper.vm.b).toBe(false)
+    expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('a && !b using Vue.nextTick', () => {
+    wrapper.vm.a = true
+    wrapper.vm.b = false
+
+    expect(wrapper.vm.a).toBe(true)
+    expect(wrapper.vm.b).toBe(false)
+    Vue.nextTick(() => {
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+})
+
+test('a && !b using wrapper.setData', () => {
+    wrapper.setData({
+      a: true,
+      b: false
+    });
 
     expect(wrapper.vm.a).toBe(true)
     expect(wrapper.vm.b).toBe(false)
@@ -37,30 +48,6 @@ test('!a && b', () => {
     expect(wrapper.html()).toMatchSnapshot()
 })
 
-// with nexttick (snapshots show correct html as expected)
-
-test('!a && !b using Vue.nextTick', () => {
-    wrapper.vm.a = false
-    wrapper.vm.b = false
-
-    expect(wrapper.vm.a).toBe(false)
-    expect(wrapper.vm.b).toBe(false)
-    Vue.nextTick(() => {
-        expect(wrapper.html()).toMatchSnapshot()
-    })
-})
-
-test('a && !b using Vue.nextTick', () => {
-    wrapper.vm.a = true
-    wrapper.vm.b = false
-
-    expect(wrapper.vm.a).toBe(true)
-    expect(wrapper.vm.b).toBe(false)
-    Vue.nextTick(() => {
-        expect(wrapper.html()).toMatchSnapshot()
-    })
-})
-
 test('!a && b using Vue.nextTick', () => {
     wrapper.vm.a = false
     wrapper.vm.b = true
@@ -68,6 +55,48 @@ test('!a && b using Vue.nextTick', () => {
     expect(wrapper.vm.a).toBe(false)
     expect(wrapper.vm.b).toBe(true)
     Vue.nextTick(() => {
-        expect(wrapper.html()).toMatchSnapshot()
+      expect(wrapper.html()).toMatchSnapshot()
     })
+})
+
+test('!a && b using wrapper.setData', () => {
+    wrapper.setData({
+      a: false,
+      b: true
+    });
+
+    expect(wrapper.vm.a).toBe(false)
+    expect(wrapper.vm.b).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('a && b', () => {
+    wrapper.vm.a = true
+    wrapper.vm.b = true
+
+    expect(wrapper.vm.a).toBe(true)
+    expect(wrapper.vm.b).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('a && b using Vue.nextTick', () => {
+    wrapper.vm.a = true
+    wrapper.vm.b = true
+
+    expect(wrapper.vm.a).toBe(true)
+    expect(wrapper.vm.b).toBe(true)
+    Vue.nextTick(() => {
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+})
+
+test('a && b using wrapper.setData', () => {
+    wrapper.setData({
+      a: true,
+      b: true
+    });
+
+    expect(wrapper.vm.a).toBe(true)
+    expect(wrapper.vm.b).toBe(true)
+    expect(wrapper.html()).toMatchSnapshot()
 })
